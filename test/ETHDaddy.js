8f8e6c3c4631a6888e227ad7f0fad699aa747c11
ishaan dhyani <ishaandhyani@ishaans-MacBook-Air.local>
2022-12-20 06:06:48 +0530

    added list doamain feature

--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -1,9 +1,50 @@
 const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("ETHDaddy", () => {
+
+  let ethDaddy;
+  let deployer,owner1;
+  const NAME="ETH Daddy";
+  const SYMBOL="ETHD";
+
+  beforeEach(async ()=>{
+    [deployer,owner1]=await ethers.getSigners();
+
+    const ETHDaddy=await ethers.getContractFactory('ETHDaddy')
+    ethDaddy=await ETHDaddy.deploy("ETH Daddy","ETHD")
+
+    const transaction=await ethDaddy.connect(deployer).list("abc.eth",tokens(10))
+    await transaction.wait()
+  })
+
+  describe("Deployment",()=>{
+    it('has a name', async ()=>{
+      const result=await ethDaddy.name()
+      expect(result).to.equal(NAME)
+    })
+    it('has a symbol', async ()=>{
+      const result=await ethDaddy.symbol()
+      expect(result).to.equal(SYMBOL)
+    })
+    it('sets the owner', async ()=>{
+      const result=await ethDaddy.owner()
+      expect(result).to.equal(deployer.address)
+    })
+  })
+
+  describe("Domain",()=>{
+    it("Returns domain attributes", async()=>{
+      let domain=await ethDaddy.domains(1);
+      expect(domain.name).to.be.equal("abc.eth");
+      expect(domain.cost).to.be.equal(tokens(10));
+      expect(domain.isOwned).to.be.equal(false);
+    })
+  })
+  
 
 })
